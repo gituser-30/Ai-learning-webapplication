31532530b4d8df750190a2745a3983291fb62f5b
gituser-30 <aryanmandhare30@gmail.com>
2026-03-19 23:53:19 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthContext } from "./Context/AuthContext";
 import "./components/global.css";
 import "./axiosConfig";
 
