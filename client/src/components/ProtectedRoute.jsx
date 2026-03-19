2247f6607edff3cc7d669e7f0407fc0f114b95df
gituser-30 <aryanmandhare30@gmail.com>
2026-03-20 00:23:23 +0530

    Update ProtectedRoute.jsx

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../Context/AuthContext";
 
 
 const ProtectedRoute = ({ children }) => {
