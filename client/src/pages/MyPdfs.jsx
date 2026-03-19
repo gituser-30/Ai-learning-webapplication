a829df60bf7e533e1b66f9f7751cc7008d5d131d
gituser-30 <aryanmandhare30@gmail.com>
2026-03-20 00:15:10 +0530

    Fix import path for AuthContext

--- client/src/pages/MyPdfs.jsx
+++ client/src/pages/MyPdfs.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../Context/AuthContext";
 import { Link } from "react-router-dom";
 const MyPdfs = () => {
   const { token } = useContext(AuthContext);
