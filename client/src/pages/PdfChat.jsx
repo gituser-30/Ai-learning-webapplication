190b4d81ed80990ac15c460b77cebb8e98dee801
gituser-30 <aryanmandhare30@gmail.com>
2026-03-20 00:14:54 +0530

    Fix import path casing for AuthContext

--- client/src/pages/PdfChat.jsx
+++ client/src/pages/PdfChat.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react";
 import axios from "axios";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../Context/AuthContext";
 import { useParams } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
