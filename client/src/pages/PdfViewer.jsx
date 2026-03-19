22000f862e48ec054ceab3d8bb94639c31c6a38b
gituser-30 <aryanmandhare30@gmail.com>
2026-03-20 00:14:36 +0530

    Update PdfViewer.jsx

--- client/src/pages/PdfViewer.jsx
+++ client/src/pages/PdfViewer.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../Context/AuthContext";
 
 const PdfViewer = () => {
   const { id } = useParams();
